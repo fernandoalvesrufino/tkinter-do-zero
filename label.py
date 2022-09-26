7135f43da6b7e1990f71cc36c52ca24170c3b7fd
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-09-25 21:58:11 -0300

    Editando a fonte, tamanho e estilo

--- label.py
+++ label.py
@@ -5,11 +5,11 @@ janela = Tk()
 janela.title('Label')
 janela.geometry('250x250')
 
-descricao = ['Nome:', 'Idade:', 'País:']
+descricao = ['Nome: ', 'Idade: ', 'País: ']
 
 for i in range(len(descricao)):
-    #defino o nome da label, onde estara (no caso, na janela), largura, altura, o que estará escrito
-    label = Label(janela, width=10, height=2, text = descricao[i])   
+    #defino o nome da label, onde estara (no caso, na janela), largura, altura, o que estará escrito, fonte
+    label = Label(janela, width=10, height=2, text = descricao[i], font=('Arial 15 bold'))   
     # aqui descrevemos qual a posicao em relacao a linha e coluna
     label.grid(row=i, column=0)     
 
